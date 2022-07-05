1a987adc3fe6d055506c8457f9059999033262eb
SageScroll18144 <lipe.fssantos063@gmail.com>
2022-07-05 20:41:36 -0300

    leitura para 'n' pinos

--- some_parts/div_ten/div_ten.ino
+++ some_parts/div_ten/div_ten.ino
@@ -2,9 +2,11 @@ const int tensao = 5;//Volts
 const double R2 = 2200;//Ohms
 double R1;
 
-void setup() {
-  
-}
+double mat_resistors[8][8];
+const int pin_piece[32];
+
+void setup() {}
+
 void loop() {
   R1 = get_resistence(A0);
 }
@@ -12,4 +14,11 @@ void loop() {
 double get_resistence(int pin){
   double U2 = tensao * analogRead(pin) / 1023;
   return tensao * R2 / U2 - R2;
+}
+void build_mat(){
+  int cnt = 0;
+  for(int i=0;i<8;i++) for(int j=0;j<8;j++) {
+    mat_resistors[i][j] = get_resistence(pin_piece[cnt]);
+    cnt++;
+  }
 }
