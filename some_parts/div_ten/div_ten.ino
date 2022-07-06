7d1895a6bc789d8ad135f12379faa7cfd01c5881
SageScroll18144 <lipe.fssantos063@gmail.com>
2022-07-06 08:32:09 -0300

    letura do tabuleiro

--- some_parts/div_ten/div_ten.ino
+++ some_parts/div_ten/div_ten.ino
@@ -2,23 +2,42 @@ const int tensao = 5;//Volts
 const double R2 = 2200;//Ohms
 double R1;
 
-double mat_resistors[8][8];
-const int pin_piece[32];
+long mat_resistors[8][8];
+long mat_copy[8][8];
+const int pin_piece[32] = {A0};
 
-void setup() {}
+bool turn = false;
+
+void setup() {
+  upd_mat();
+  last_state();  
+}
 
 void loop() {
-  R1 = get_resistence(A0);
+  upd_mat();
+  if(hasAMovement()){
+    last_state();
+    //Envia para a I.A.
+  }
 }
 
-double get_resistence(int pin){
+long get_resistence(int pin){
   double U2 = tensao * analogRead(pin) / 1023;
-  return tensao * R2 / U2 - R2;
+  return (long)(tensao * R2 / U2 - R2);
 }
-void build_mat(){
+void upd_mat(){
   int cnt = 0;
   for(int i=0;i<8;i++) for(int j=0;j<8;j++) {
     mat_resistors[i][j] = get_resistence(pin_piece[cnt]);
     cnt++;
   }
+}
+void last_state(){for(int i=0;i<8;i++) for(int j=0;j<8;j++) mat_copy[i][j]=mat_resistors[i][j];}
+bool hasAMovement(){
+  for(int i=0;i<8;i++) for(int j=0;j<8;j++) {
+    if(mat_copy[i][j]!=mat_resistors[i][j]){
+      return true;
+    }
+  }
+  return false;
 }
