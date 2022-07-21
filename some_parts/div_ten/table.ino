d77361879b7c09e28a1e0390236cb7ade82478f9
Felipe Santos <lipe.fssantos063@gmail.com>
2022-07-21 16:14:20 -0300

    Update table.ino

--- some_parts/div_ten/table.ino
+++ some_parts/div_ten/table.ino
@@ -1,5 +1,6 @@
 double get_resistence(int pin){
   double U2 = voltage * analogRead(pin) / 1023;
+  if(U2==0.0) return 0.0;
   return ((voltage * R2 / U2) - R2);
 }
 void build_list_of_piece(){
@@ -17,7 +18,7 @@ void build_list_of_piece(){
   n.whichone = 'b';
   pieces[2] = n;
 
-  n.value = 680.0;
+  n.value = 8200.0;
   n.whichone = 'k';
   pieces[3] = n;
 
@@ -33,11 +34,11 @@ void build_list_of_piece(){
   n.whichone = 'T';
   pieces[6] = n;
 
-  n.value = 6800.0;
+  n.value = 22000.0;
   n.whichone = 'C';
   pieces[7] = n;
 
-  n.value = 8200.0;
+  n.value = 680.0;
   n.whichone = 'B';
   pieces[8] = n;
 
@@ -54,13 +55,13 @@ void build_list_of_piece(){
   pieces[11] = n;
 }
 char which_piece(double res_value){
+  if(res_value == 0.0) return 'x';
   for(int i=0;i<12;i++){
     double x = pieces[i].value;
     if(res_value>=0.85*x && res_value<=1.15*x){
       return pieces[i].whichone;
     }
   }
-  return 'x';
 }
 void print_piece(int pin){
   Serial.print(which_piece(get_resistence(pin)));
