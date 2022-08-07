914981868736e7523e80ffa71c4819437a37b500
SageScroll18144 <lipe.fssantos063@gmail.com>
2022-08-07 20:11:15 -0300

    correção na tabela analogica

--- some_parts/div_ten/table.ino
+++ some_parts/div_ten/table.ino
@@ -1,6 +1,5 @@
 double get_resistence(int pin){
   double U2 = voltage * analogRead(pin) / 1023;
-  if(U2==0.0) return 0.0;
   return ((voltage * R2 / U2) - R2);
 }
 double get_analog_resistence(int pin){
@@ -22,11 +21,7 @@ void build_list_of_piece(){
   n.whichone = 'b';
   pieces[2] = n;
 
-<<<<<<< HEAD
-  n.value = 607;//680.0
-=======
-  n.value = 8200.0;
->>>>>>> d77361879b7c09e28a1e0390236cb7ade82478f9
+  n.value = 109;//8200.0
   n.whichone = 'k';
   pieces[3] = n;
 
@@ -42,19 +37,11 @@ void build_list_of_piece(){
   n.whichone = 'T';
   pieces[6] = n;
 
-<<<<<<< HEAD
-  n.value = -1;//6800.0
+  n.value = 43;//22000.0
   n.whichone = 'C';
   pieces[7] = n;
 
-  n.value = 109;//8200.0
-=======
-  n.value = 22000.0;
-  n.whichone = 'C';
-  pieces[7] = n;
-
-  n.value = 680.0;
->>>>>>> d77361879b7c09e28a1e0390236cb7ade82478f9
+  n.value = 607;//680.0
   n.whichone = 'B';
   pieces[8] = n;
 
@@ -62,11 +49,7 @@ void build_list_of_piece(){
   n.whichone = 'K';
   pieces[9] = n;
 
-<<<<<<< HEAD
-  n.value = 77;//1200.0
-=======
-  n.value = 12000.0;
->>>>>>> d77361879b7c09e28a1e0390236cb7ade82478f9
+  n.value = 77;//12000.0
   n.whichone = 'Q';
   pieces[10] = n;
 
@@ -75,13 +58,13 @@ void build_list_of_piece(){
   pieces[11] = n;
 }
 char which_piece(double res_value){
-  if(res_value == 0.0) return 'x';
   for(int i=0;i<12;i++){
     double x = pieces[i].value;
     if(res_value>=0.95*x && res_value<=1.05*x){
       return pieces[i].whichone;
     }
   }
+  return 'x';
 }
 void print_piece(int pin){
   Serial.print(which_piece(get_analog_resistence(pin)));
