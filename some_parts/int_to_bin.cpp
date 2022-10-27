58b3713630cdb4bf2b3fbe485a10f4ec812a3a14
Felipe Santos <ffss@cin.ufpe.br>
2022-10-27 15:52:09 -0300

    converte int para bin

---
+++ some_parts/int_to_bin.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int num=5; //Assuming 16 bit integer
+    for(int i=15; i>=0; i--) cout<<((num >> i) & 1);
+    cout<<endl;
+    for(int i=0; i<16; i++) cout<<((num >> i) & 1);
+    cout<<endl;
+    
+    return 0;
+}
